8e18199be81f575937bbd1551f151ccf984891c3
Jennie <emomoop@gmail.com>
2015-03-04 09:38:01 -0800

    Edited Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'jennie-bloccitoff.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
 end
