4a6e3bd63917f26ccccd159f08fc6f0d4bdf69c7
Jennie <emomoop@gmail.com>
2015-03-25 10:14:44 -0700

    Finished creating to-do items, cleaned up code, deleted unnecessary code, and created a couple to-do items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,29 @@
 class ItemsController < ApplicationController
-  
-  def new 
-    @items = Item.new
+
+  # def new 
+  #   @item = Item.new
+  # end
   
   def create 
-    @items = current_user.items.build
-    authorize @items
-    if @items.save
-      redirect_to items_path, notice: 'Your item has been saved.'
+    @user = User.find params[:user_id]
+    @item = @user.items.build( item_params )
+
+    #authorize @item
+
+    if @item.save
+      redirect_to user_path(@user), notice: 'Your item has been saved.'
     else
       flash[:error] = 'There was an error saving your item.'
       render :new
-    end
+    end 
+
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name)
+
   end
 
 end
